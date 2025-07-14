25ce22062dfc147c92df1d9452487c4e08d5b508
HendrikRauh <114620133+HendrikRauh@users.noreply.github.com>
2025-07-14 12:02:09 +0200

    OTA updates Fixes #6

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <Sensor.h>
 #include <server.h>
 
+#include <ArduinoOTA.h>
+#include <Update.h>
+
 char macStr[18] = {0};
 
 void setup()
@@ -46,11 +49,32 @@ void setup()
   }
 
   Serial.printf("[MASTER_DEBUG] Setup abgeschlossen - Status: %s\n", masterStatusToString(getMasterStatus()).c_str());
+
+  // OTA-Initialisierung
+  ArduinoOTA.setHostname("zeitnahme");
+  ArduinoOTA.onStart([]()
+                     {
+    String type = ArduinoOTA.getCommand() == U_FLASH ? "sketch" : "filesystem";
+    Serial.println("[OTA] Start updating " + type); });
+  ArduinoOTA.onEnd([]()
+                   { Serial.println("[OTA] End"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("[OTA] Progress: %u%%\r", (progress / (total / 100))); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
+    Serial.printf("[OTA] Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
+  ArduinoOTA.begin();
 }
 
 void loop()
 {
   // Die Hauptlogik läuft jetzt in separaten Tasks
   // Minimale Verzögerung für bessere Performance
+  ArduinoOTA.handle();
   delay(100);
 }
